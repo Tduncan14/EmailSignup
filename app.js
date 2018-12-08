61b09c9db91c5c0ee7d84132338dc11848c8243d
unknown <nbatre1@yahoo.com>
2018-12-07 21:57:03 -0600

    keep moving forward

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const request = require('request');
-const bodyParser = require('body-parse');
+const bodyParser = require('body-parser');
 const path = require('path');
+
+
+const app = express();
+
+// Static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const PORT= process.env.PORT || 5002;
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
