b2c97f1c76d6c16c9cc028f43d89a1e8134bae90
unknown <nbatre1@yahoo.com>
2018-12-08 20:14:46 -0600

    keep moving forward

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const path = require('path');
 
 const app = express();
 
+// Bodyparser Middleware
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Static folder
 app.use(express.static(path.join(__dirname,'public')));
+
+//signup Route cause it is a post that i am handling
+
+app.post('/signup',(req,res)=>{
+
+})
 
 
 const PORT= process.env.PORT || 5002;
